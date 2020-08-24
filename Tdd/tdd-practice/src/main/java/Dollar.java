34342a971dff3f5e0e34072ef29a8c97a26320c0
mgmix <evace93@gmail.com>
2020-08-24 19:02:55 +0900

    feat: 1-4 장 연습 Dollar Test 추가

---
+++ Tdd/tdd-practice/src/main/java/Dollar.java
@@ -0,0 +1,21 @@
+public class Dollar {
+
+    private int amount;
+
+    Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Dollar dollar = (Dollar) obj;
+        return amount == dollar.amount;
+    }
+
+
+
+}
